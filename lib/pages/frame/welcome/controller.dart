1a2792d7c3348a8ad52e5f150a17d53d601ac247
Jean <acalto2@gmail.com>
2023-10-18 20:16:05 +0200

    6. Welcome module and binding controller

---
+++ lib/pages/frame/welcome/controller.dart
@@ -0,0 +1,15 @@
+import 'package:get/get.dart';
+
+import 'state.dart';
+
+class WelcomeController extends GetxController {
+  WelcomeController();
+  final title = "Chatty .";
+  final state = WelcomeState();
+
+  @override
+  void onReady() {
+    super.onReady();
+    print("WelcomeController");
+  }
+}
